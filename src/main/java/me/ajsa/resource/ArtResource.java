751f0f127eeb4a99e90462a6d3bc3adb482196df
ajsadcc <ajsaajsica2004@gmail.com>
2025-05-07 14:34:10 +0200

    1.4

--- src/main/java/me/ajsa/resource/ArtResource.java
+++ src/main/java/me/ajsa/resource/ArtResource.java
@@ -1,14 +1,22 @@
 package me.ajsa.resource;
 
+import java.util.List;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import me.ajsa.exception.ArtException;
 import me.ajsa.model.Art;
-import me.ajsa.repository.ArtRepository;
-
-import java.util.List;
+import me.ajsa.model.client.ArtTimeMetrics;
 
 @Path("/art/")
 public class ArtResource {
@@ -16,17 +24,23 @@ public class ArtResource {
     @Inject
     private ArtRepository artRepository;
 
+    @RestClient
+    private ArtTimeMetrics timeClient;
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    @Path("create")
+    @Path("/addArt")
     public Response createArt(Art art) {
-        Art a = artRepository.addArt(art);
+        Art a = artRepository.createArt(art);
         return Response.ok().entity(a).build();
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("getAll")
+    @Path("getAllArts")
     public Response getAllArts() {
         List<Art> arts = artRepository.getAllArts();
         return Response.ok().entity(arts).build();
@@ -34,14 +48,30 @@ public class ArtResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    @Path("get")
-    public Response getArtByTitle(@QueryParam("title") String title) {
+    @Path("getArtsByName")
+    public Response getArtsByName(@QueryParam(value = "name") String name) {
         List<Art> arts;
         try {
-            arts = artRepository.getArtByTitle(title);
+            arts = artRepository.getArtsByName(name);
         } catch (ArtException e) {
-            return Response.status(Response.Status.NOT_FOUND).entity(e.getMessage()).build();
+            return Response.ok().entity(e.getMessage()).build();
         }
         return Response.ok().entity(arts).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("getTime")
+    public Response getTime(@QueryParam(value = "timeZone") String timeZone) {
+        ArtTimeMetrics time = timeClient.getTime(timeZone);
+        return Response.ok().entity(time).build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("createArtArtist")
+    public Response createArtArtist(ArtArtist a) {
+        ArtArtist artArtist = artRepository.createArtArtist(a);
+        return Response.ok().entity(artArtist).build();
     }
 }
---
+++ src/main/java/me/ajsa/sheduel/SchedulerUtil.java
@@ -0,0 +1,20 @@
+package me.ajsa.sheduel;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import io.quarkus.scheduler.Scheduled;
+import me.ajsa.model.client.ArtTimeMetrics;
+import me.ajsa.restclient.TimeClient;
+
+public class SchedulerUtil {
+
+    @RestClient
+    TimeClient timeClient;
+
+    //@Scheduled(every="1s")
+    void getTime() {
+        ArtTimeMetrics time = timeClient.getTime("Europe/Amsterdam");
+        System.out.println(time);
+    }
+
+}
